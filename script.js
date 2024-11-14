032571a993f1549969eaa2b79ec835c09cddfbc0
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-11-14 20:23:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const audio = document.getElementById('audio');
 // Array of track sources
 const tracks = [
     'music/Let Her Go x Husn (Gravero Mashup)  Anuv Jain.mp3',
-    'music/Hasi (Audio).mp3',
     'music/laree Choote lyrics Original.mp3',
-    'music/Safar Mashup  Harshal Music  Safar X Stay X Kina Chir  The Prophec  Juss  Punjabi Mashup 2023.mp3'
+    'music/Safar Mashup  Harshal Music  Safar X Stay X Kina Chir  The Prophec  Juss  Punjabi Mashup 2023.mp3',
+    'music/Hasi (Audio).mp3',
 ];
 
 function playPause() {
